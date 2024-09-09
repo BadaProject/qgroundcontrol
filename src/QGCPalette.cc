b29eb6f24ef30874bbd0a5606fdad59ab7a0c5e3
jeyong <jeyong@gmail.com>
2024-09-10 00:10:35 +0900

    change logo

--- src/QGCPalette.cc
+++ src/QGCPalette.cc
@@ -85,7 +85,7 @@ void QGCPalette::_buildMap()
 
     // Colors not affecting by theming
     //                                              Disabled    Enabled
-    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple,     "#4A2C6D", "#4A2C6D")
+    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple,     "#2373EAD6", "#1278E5")
     DECLARE_QGC_NONTHEMED_COLOR(brandingBlue,       "#48D6FF", "#6045c5")
     DECLARE_QGC_NONTHEMED_COLOR(toolStripFGColor,   "#707070", "#ffffff")
 
